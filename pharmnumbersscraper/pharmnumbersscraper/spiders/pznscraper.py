a6e94822ca2395498ebc96f02b279f852a683a7b
Amara <148555486+HiAmara@users.noreply.github.com>
2023-12-12 11:28:21 +0100

    code for pzn scraping works now

--- pharmnumbersscraper/pharmnumbersscraper/spiders/pznscraper.py
+++ pharmnumbersscraper/pharmnumbersscraper/spiders/pznscraper.py
@@ -1,43 +1,50 @@
 import scrapy
 import pandas as pd
-from pharmnumbersscraper.items import HealthProductItem
+import openpyxl
+import fsspec
+
 from urllib.parse import quote
 from scrapy.http import FormRequest
 
 # our first spider
 class PznscraperSpider(scrapy.Spider):
     name = "pznscraper"
-    allowed_domains = ["www.dkv.com"]
-    start_urls = ["http://www.dkv.com/"]
-
-    #path to excel file which contains the pzn numbers
-    excel_path = "C://Users//jinji//Desktop//Data Science//Programming Projects//webscrapy//data files//testdata_pzn.xlsx"
-    df = pd.read_excel(excel_path)
-    numbers = df['PZN'].tolist()
-
-
+    allowed_domains = ['dkv.com']
 
     def start_requests(self):
-        # Iterate through numbers and create requests
-        for number in self.numbers:
-            yield scrapy.Request(url=f'https://www.dkv.com/gesundheit-arzneimittel-preis-vergleich.html?start=1&init=1&cat=pzn&q={quote(number)}#preisvergleich', callback=self.parse)
+        # Load Excel data
+        excel_path = r'C:\Users\jinji\Desktop\Data Science\Programming Projects\webscrapy\data files\testdata_pzn.xlsx'
+        df = pd.read_excel(excel_path)
+
+        # Iterate through PZN column and generate requests
+        for pzn_number in df['PZN']:
+            url = f'https://www.dkv.com/gesundheit-arzneimittel-preis-vergleich.html?start=1&init=1&cat=pzn&q={pzn_number}#preisvergleich'
+            yield scrapy.Request(url, callback=self.parse, meta={'pzn_number': pzn_number})
+            
+
 
 
     def parse(self, response):
+        
         # Extract the price using the provided CSS selector
-        price = response.css('#arzneidetails > table.comparemed > tbody > tr:nth-child(1) > td.price::text').get()
+        price = response.css('div#arzneidetails table.comparemed tr td.price').get()
 
-        # Update the DataFrame with the extracted price
-        number = response.meta['number']
-        row_index = self.df[self.df['PZN'] == number].index[0]
-        self.df.at[row_index, 'Price'] = price
+        # Extract PZN number from meta
+        pzn_number = response.meta['pzn_number']
 
-        # Save the updated DataFrame back to the Excel file
-        self.df.to_excel('path/to/your/excel/file_updated.xlsx', index=False)
+        # Update the Excel sheet with the price information
+        self.update_excel(pzn_number, price)
+
+    def update_excel(self, pzn_number, price):
+        # Load Excel data
+        excel_path = r'C:\Users\jinji\Desktop\Data Science\Programming Projects\webscrapy\data files\testdata_pzn.xlsx'
+        df = pd.read_excel(excel_path)
+
+        # Update the second column with the price
+        df.loc[df['PZN'] == pzn_number, 'Price'] = price
+
+        # Save the updated DataFrame to Excel
+        df.to_excel(excel_path, index=False)
 
 
-    ##functions i need
-    # puts pzn into search bar and clicks on search
-    # css path or xpath to price
-    # add price to excel file
-
+   
